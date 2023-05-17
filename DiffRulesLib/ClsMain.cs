20ac154538c34569e16e3125a4561bb5242cba07
JR-Fuentes-Martinez <jrfm.verboran@outlook.es>
2023-05-17 02:52:02 +0200

    20230517T02:50

--- DiffRulesLib/ClsMain.cs
+++ DiffRulesLib/ClsMain.cs
@@ -1,93 +1,84 @@
-﻿using Microsoft.Data.Analysis;
-using System.Text;
+﻿using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace DiffRulesLib;
 
 public class ClsMain
 {
-    public DataFrame InDatosSorted {get; set;} = null;
-    public double Clave1 {get; set;} = double.NaN;
-    public double Clave2 {get; set;} = double.NaN;
-    public DataFrame Serie {get; set;} = null;
-    public string NClave1 {get; set;} = string.Empty;
-    public string NClave2 {get; set;} = string.Empty;
+    public void Make_Diff_Breve_T_S(ref DataFrame InDatosSorted, int Multiplo)
+    { 
+        var CulUsa = new System.Globalization.CultureInfo("en-US");   
+        if (InDatosSorted.Serie.Length != InDatosSorted.ColumnasIn.Count) return;
+        if (InDatosSorted == null || InDatosSorted.Serie == null) return;
+        if (InDatosSorted.ColumnasIn.Count < 1) return;
+        if (InDatosSorted.Serie.Length < 1)   return;
+        var NumSeries = InDatosSorted.Serie.Length;
 
-    private List<DataFrame> Make_Diff_Breve_T(int Multiplo)
-    {       
-        long NumColumnas = InDatosSorted.Columns.Count;
-        long NumFilas = InDatosSorted.Rows.Count;
-        if (NumColumnas < 1) return null;
-        List<DataFrame> outDatos = new(2);        
-        
-        for (int i = 0; i < NumColumnas; i++)
+        for (int s = 0; s < NumSeries; s++)
         {
-            double[] Yes = new double[NumFilas];
-            double[] Xes = new double[NumFilas];
-            double[] X2es = new double[NumFilas * Multiplo];
-            double[] f, d;
+            var NumColumnas = InDatosSorted.ColumnasIn.Count;        
+            var Columna = InDatosSorted.ColumnasIn[s];
+            var NumFilas = Columna[s].Length;
+            var ColumnaOut = new List<double[]>(NumFilas * Multiplo);
+            double[] X2es = new double[NumFilas * Multiplo];            
 
-            for (int j = 0; j < NumFilas; j++)
-            {
-                Yes[j] = (double)InDatosSorted[j,i];
-                Xes[j] = j;
-            }            
             for (int j = 0; j < NumFilas * Multiplo; j++) {
                 X2es[j] = (double)j / (double)Multiplo;
-            }
-            alglib.spline1dconvdiffcubic(Xes, Yes, Xes.Length, 0, 0.0, 0, 0.0, X2es, X2es.Length, out f, out d);
-            double[] ffromd = new double[X2es.Length];
-            double[] ferror = new double[X2es.Length];
-            double Acunm = 0.0;
-
-            for (int j = 0; j < ffromd.Length; j++)
-            {
-                Acunm += f[j];
-                ffromd[j] = Acunm;
-                if (f[j] != 0) ferror[j] = (double)ffromd[j] / (double)f[j];
-                    else ferror[j] = double.NaN;
             }            
-            outDatos[0].Columns.Add(new DoubleDataFrameColumn(InDatosSorted.Columns[i].Name, ffromd)); 
-            outDatos[0].Columns.Add(new DoubleDataFrameColumn("Ratio fd/f", ferror)); 
-            outDatos[1].Columns.Add(new DoubleDataFrameColumn(InDatosSorted.Columns[i].Name, d));
+            for (int i = 0; i < NumColumnas; i++)
+            {
+                double[] Yes = new double[NumFilas];
+                double[] Xes = new double[NumFilas];
+                double[] f, d;
+
+                for (int j = 0; j < NumFilas; j++)
+                {
+                    Yes[j] = Columna[i][j];
+                    Xes[j] = j;
+                }                
+                alglib.spline1dconvdiffcubic(Xes, Yes, Xes.Length, 0, 0.0, 0, 0.0, X2es, X2es.Length, out f, out d);
+                double[] ffromd = new double[X2es.Length];
+                double[] ferror = new double[X2es.Length];
+                double Acum = f[0];
+
+                for (int j = 0; j < ffromd.Length; j++)
+                {
+                    Acum += d[j];
+                    ffromd[j] = Acum;
+                    if (f[j] != 0) ferror[j] = (double)ffromd[j] / (double)f[j];
+                        else ferror[j] = double.MinValue;
+                }
+                ColumnaOut.Add(ffromd);
+                ColumnaOut.Add(ferror);
+                ColumnaOut.Add(d);
+            }
+            InDatosSorted.ColumnasOut.Add(ColumnaOut);
         }
-        return outDatos;        
+        InDatosSorted.Procesado = true;
     }
 
-    public bool Make_Diff_Breve_T_S( string Ruta = "", int Multiplo = 1)
+    public void Persist_DF_Json(ref DataFrame InDatosSorted, string Ruta = "")
     {
-        if (InDatosSorted == null || Serie == null) return false;
-        if (InDatosSorted.Columns.Count < 1 || InDatosSorted.Rows.Count < 1) return false;
-        if (Serie.Columns.Count < 2 || Serie.Rows.Count < 1) return false;
-        List<DataFrame> outDatos =  new(2);
-        var CulUsa = new System.Globalization.CultureInfo("en-US");
-        
-        for (int i = 0; i < Serie.Rows.Count; i++)        
-        {
-            var Datos = Make_Diff_Breve_T(Multiplo);
-            var NumFilas = Datos[0].Rows.Count;
-
-            Datos[0].Columns.Add(new DoubleDataFrameColumn(NClave1, NumFilas));
-            Datos[0].Columns[NClave1].FillNulls(Clave1, true);
-            Datos[1].Columns.Add(new DoubleDataFrameColumn(NClave1, NumFilas));
-            Datos[1].Columns[NClave1].FillNulls(Clave1, true);
-            Datos[0].Columns.Add(new DoubleDataFrameColumn(NClave2, NumFilas));
-            Datos[0].Columns[NClave2].FillNulls(Clave2, true);
-            Datos[1].Columns.Add(new DoubleDataFrameColumn(NClave1, NumFilas));
-            Datos[1].Columns[NClave2].FillNulls(Clave2, true);
-
-            Datos[0].Columns.Add(new DoubleDataFrameColumn(Serie[1, i].ToString(), NumFilas));
-            Datos[0].Columns[Serie[1, i].ToString()].FillNulls(Serie[0, i], true);
-            Datos[1].Columns.Add(new DoubleDataFrameColumn(Serie[1, i].ToString(), NumFilas));
-            Datos[1].Columns[Serie[1, i].ToString()].FillNulls(Serie[0, i], true);   
-            outDatos[0].Append(Datos[0].Rows, true);
-            outDatos[1].Append(Datos[1].Rows, true);
-        }
         try {
-            DataFrame.SaveCsv(outDatos[0], Ruta, ';', true, Encoding.UTF8, CulUsa);        
-            DataFrame.SaveCsv(outDatos[1], Ruta, ';', true, Encoding.UTF8, CulUsa);  
-            return true;      
+            InDatosSorted.Salvado = true;
+            var cadena = JsonSerializer.Serialize(value: InDatosSorted, typeof(DataFrame));
+            File.WriteAllText(Ruta, cadena);
         } catch {
-            return false;
+            InDatosSorted.Salvado = false;
         }
     }
+}
+public class DataFrame
+{
+    public double[] Serie {get; set;}
+    public List<List<double[]>> ColumnasIn {get; set;} = new();
+    public List<List<double[]>> ColumnasOut {get; set;} = new();
+    public double Clave1Double {get; set;} = 0.0;
+    public double Clave2Double {get; set;} = 0.0;
+    public DateTime Clave3DateTime {get; set;} = DateTime.MinValue;
+    public string Coletilla {get; set;} = string.Empty;    
+    public bool Procesado {get; set;} = false;
+    public bool Salvado {get; set;} = false;
+
 }
--- test/Program.cs
+++ test/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DiffRulesLib;
+
+// See https://aka.ms/new-console-template for more information
+
+double[] Numeros = new double[400];
+for (int i = 0; i < Numeros.Length; i++) {
+    Numeros[i] = 5.0 * Math.Pow(i, 2);    
+}
+List<double[]> Lista = new() {
+    Numeros, Numeros, Numeros
+};
+List<List<double[]>> Todas = new() {Lista, Lista, Lista};
+
+double[] LaSerie = new double[] {1, 2, 3};
+DataFrame LosDatos = new DataFrame() {
+    Clave1Double = 0.0,
+    Clave2Double = 0.0,
+    Clave3DateTime = DateTime.Now,
+    Serie = LaSerie,
+    ColumnasIn = Todas,
+    Coletilla = string.Empty
+};
+var Clase = new ClsMain();
+Clase.Make_Diff_Breve_T_S(ref LosDatos,  1);
+Clase.Persist_DF_Json(ref LosDatos, "/home/jr-fuentes-martinez/Documentos/DiffRules/persist.html");
+
+var a = 1;
+
