dbf052935b18a16ccb47218e60af2677b1ebded5
JR-Fuentes-Martinez <jrfm.verboran@outlook.es>
2023-05-20 00:31:21 +0200

    20230520T99:31

--- DiffRulesLib/ClsMain.cs
+++ DiffRulesLib/ClsMain.cs
@@ -1,84 +1,119 @@
 ﻿using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using Plotly.NET.CSharp;
+using Plotly.NET.ConfigObjects;  
+using Plotly.NET.LayoutObjects;
+using Plotly.NET.TraceObjects;
 
 namespace DiffRulesLib;
 
+public enum TipoFuncion
+{
+    FuncionPrimitiva,
+    Derivada,
+    Error
+}
 public class ClsMain
 {
-    public void Make_Diff_Breve_T_S(ref DataFrame InDatosSorted, int Multiplo)
+    public void Make_Diff_Breve_T_S(ref DataFrame InDatosSorted, int Multiplo = 1)
     { 
-        var CulUsa = new System.Globalization.CultureInfo("en-US");   
-        if (InDatosSorted.Serie.Length != InDatosSorted.ColumnasIn.Count) return;
-        if (InDatosSorted == null || InDatosSorted.Serie == null) return;
-        if (InDatosSorted.ColumnasIn.Count < 1) return;
-        if (InDatosSorted.Serie.Length < 1)   return;
-        var NumSeries = InDatosSorted.Serie.Length;
+        var CulUsa = new System.Globalization.CultureInfo("en-US");  
+        if (InDatosSorted == null ) return; 
+        if (Multiplo < 1 || Multiplo > 30) return;
+        var NumFechas = InDatosSorted.DF.Count;      
 
-        for (int s = 0; s < NumSeries; s++)
-        {
-            var NumColumnas = InDatosSorted.ColumnasIn.Count;        
-            var Columna = InDatosSorted.ColumnasIn[s];
-            var NumFilas = Columna[s].Length;
-            var ColumnaOut = new List<double[]>(NumFilas * Multiplo);
-            double[] X2es = new double[NumFilas * Multiplo];            
+        for (int j = 0; j < NumFechas; j++) {
+            var NumMags = InDatosSorted.DF[j].Datos.Count;
 
-            for (int j = 0; j < NumFilas * Multiplo; j++) {
-                X2es[j] = (double)j / (double)Multiplo;
-            }            
-            for (int i = 0; i < NumColumnas; i++)
-            {
-                double[] Yes = new double[NumFilas];
-                double[] Xes = new double[NumFilas];
+            for (int k = 0; k < NumMags; k++) {
+                var NumMag = InDatosSorted.DF[j].Datos[k].Mag.Length;
+
+                double[] X2es = new double[NumMag * Multiplo];
+                double[] Xes = new double[NumMag];
+                double[]? Yes = InDatosSorted.DF[j].Datos[k].Mag;
                 double[] f, d;
 
-                for (int j = 0; j < NumFilas; j++)
+                for (int l = 0; l < Yes.Length; l++)
                 {
-                    Yes[j] = Columna[i][j];
-                    Xes[j] = j;
-                }                
+                    Xes[l] = l;
+                }
+                for (int l = 0; l < X2es.Length; l++) {
+                    X2es[l] = (double)l / (double)Multiplo;
+                }                    
                 alglib.spline1dconvdiffcubic(Xes, Yes, Xes.Length, 0, 0.0, 0, 0.0, X2es, X2es.Length, out f, out d);
                 double[] ffromd = new double[X2es.Length];
                 double[] ferror = new double[X2es.Length];
-                double Acum = f[0];
-
-                for (int j = 0; j < ffromd.Length; j++)
-                {
-                    Acum += d[j];
-                    ffromd[j] = Acum;
-                    if (f[j] != 0) ferror[j] = (double)ffromd[j] / (double)f[j];
-                        else ferror[j] = double.MinValue;
+                var Acum = f[0];
+                ffromd[0] = Acum;
+            
+                for (int l = 1; l < X2es.Length; l++)
+                {   
+                    Acum += d[l];
+                    ffromd[l] = Acum;
+                    if (f[l] != 0) ferror[l] = (double)ffromd[l] / (double)f[l];
+                        else ferror[l] = double.MinValue;
                 }
-                ColumnaOut.Add(ffromd);
-                ColumnaOut.Add(ferror);
-                ColumnaOut.Add(d);
-            }
-            InDatosSorted.ColumnasOut.Add(ColumnaOut);
+                InDatosSorted.DF[j].Datos[k].Derivada = d;
+                InDatosSorted.DF[j].Datos[k].PrimitivaPorAcum = ffromd;
+                InDatosSorted.DF[j].Datos[k].Error = ferror;
+                InDatosSorted.DF[j].Datos[k].Tiempo = X2es;
+            }                            
         }
-        InDatosSorted.Procesado = true;
+        InDatosSorted.Procesado = true;    
     }
-
-    public void Persist_DF_Json(ref DataFrame InDatosSorted, string Ruta = "")
+    public bool Persist_DF_Json(ref DataFrame InDatosSorted, string Ruta)
     {
         try {
             InDatosSorted.Salvado = true;
             var cadena = JsonSerializer.Serialize(value: InDatosSorted, typeof(DataFrame));
             File.WriteAllText(Ruta, cadena);
+            return true;
         } catch {
-            InDatosSorted.Salvado = false;
+            return false;
         }
+    }    
+    public Plotly.NET.GenericChart.GenericChart Make_Plotly_3DGraph(ref DataFrame InDatosSorted, int Serie, 
+        int Mag, TipoFuncion Func = TipoFuncion.Derivada  ) {    
+
+        double[] FuncionZ;
+
+        if (Func == TipoFuncion.Derivada) {
+            FuncionZ = InDatosSorted.DF[Serie].Datos[Mag].Derivada;
+        } else if (Func == TipoFuncion.FuncionPrimitiva) {
+            FuncionZ = InDatosSorted.DF[Serie].Datos[Mag].PrimitivaPorAcum;
+        } else {
+            FuncionZ = InDatosSorted.DF[Serie].Datos[Mag].Error;
+        }
+        
+        var Grafico = Chart.Scatter3D<double,double,double,string>(
+            InDatosSorted.Series, InDatosSorted.DF[Serie].Datos[Mag].Tiempo,
+            FuncionZ, Plotly.NET.StyleParam.Mode.Lines, ShowLegend: false);
+            //, LineColor: new Optional<Plotly.NET.Color>(Plotly.NET.Color.fromString("blue"),true), 
+            //LineWidth: 2, Text: "Evolución Breve_T_S");public
+        return Grafico;
     }
 }
-public class DataFrame
+
+public class DataSerie
 {
-    public double[] Serie {get; set;}
-    public List<List<double[]>> ColumnasIn {get; set;} = new();
-    public List<List<double[]>> ColumnasOut {get; set;} = new();
-    public double Clave1Double {get; set;} = 0.0;
-    public double Clave2Double {get; set;} = 0.0;
-    public DateTime Clave3DateTime {get; set;} = DateTime.MinValue;
-    public string Coletilla {get; set;} = string.Empty;    
+    public double[] Mag {get; set;}
+    public double[] Derivada {get; set;} 
+    public double[] PrimitivaPorAcum {get; set;}
+    public double[] Error {get; set;} 
+    public double[] Tiempo {get; set;}
+}
+public class DataSeries
+{
+    public double Serie {get; set;} = double.MinValue;
+    public List<DataSerie> Datos {get; set;} = new();    
+}
+public class DataFrame
+{    
+    public double Clave1 {get; set;} = double.MinValue;
+    public double Clave2 {get; set;} = double.MinValue;
+    public List<DataSeries> DF {get; set;} = new();
+    public double[] Series {get; set;}
     public bool Procesado {get; set;} = false;
     public bool Salvado {get; set;} = false;
-
 }
--- test/Program.cs
+++ test/Program.cs
@@ -1,28 +1,32 @@
 ﻿using DiffRulesLib;
+using Plotly.NET.CSharp;
 
-// See https://aka.ms/new-console-template for more information
-
-double[] Numeros = new double[400];
-for (int i = 0; i < Numeros.Length; i++) {
-    Numeros[i] = 5.0 * Math.Pow(i, 2);    
+var Magnitud = new double[20];
+for (int i = 0; i < Magnitud.Length; i++)
+{
+    Magnitud[i] = Math.Pow(i, 2);
 }
-List<double[]> Lista = new() {
-    Numeros, Numeros, Numeros
-};
-List<List<double[]>> Todas = new() {Lista, Lista, Lista};
+var Datos = new DataFrame();
+Datos.Clave1 = 0.0;
+Datos.Clave2 = 0.0;
+Datos.Series = new double[] {1.0, 2.0, 3.0};
 
-double[] LaSerie = new double[] {1, 2, 3};
-DataFrame LosDatos = new DataFrame() {
-    Clave1Double = 0.0,
-    Clave2Double = 0.0,
-    Clave3DateTime = DateTime.Now,
-    Serie = LaSerie,
-    ColumnasIn = Todas,
-    Coletilla = string.Empty
-};
+for (int j = 0; j < 3; j++) {
+    var DS = new DataSeries();
+
+    for (int i = 0; i < 2; i++)
+    {
+        DataSerie Data1 = new DataSerie() {
+            Mag =Magnitud
+        };
+        DS.Datos.Add(Data1);
+    }
+    DS.Serie = Datos.Series[j];
+    Datos.DF.Add(DS);
+}
 var Clase = new ClsMain();
-Clase.Make_Diff_Breve_T_S(ref LosDatos,  1);
-Clase.Persist_DF_Json(ref LosDatos, "/home/jr-fuentes-martinez/Documentos/DiffRules/persist.html");
-
-var a = 1;
+Clase.Make_Diff_Breve_T_S(ref Datos,  1);
+Clase.Persist_DF_Json(ref Datos, "/home/jr-fuentes-martinez/Documentos/DiffRules/persist.json");
+//var Grafico = Clase.Make_Plotly_3DGraph(ref LosDatos, 0, 0);
+//Grafico.Show();
 
